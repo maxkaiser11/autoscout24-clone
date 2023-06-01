3926354aa66556b0af49e2f4df26b64f830271ff
rodrigo kaufmann <rodrigorkauf@gmail.com>
2023-06-01 12:47:13 +0200

    search bar implemented

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,13 @@
 class Vehicle < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   has_many :reservations
   validates :type_of_vehicle, :title, :price_per_day, :location, :brand, :model, :description, :year, presence: true
+
+  pg_search_scope :search_by_model_and_brand,
+  against: [ :model, :brand ],
+  using: {
+    tsearch: { prefix: true }
+  }
+
 end
